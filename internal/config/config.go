9f6797682640edb9518e3b74260e17e1a4e04023
Rob Brazier <git+github@brzr.co>
2024-12-06 21:13:43 +0000

    feat(docker): add dockerfile for goreleaser

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,26 +3,26 @@ package config
 type Config struct {
 	Columns   ColumnConfig   `yaml:"columns"`
 	Databases DatabaseConfig `yaml:"databases"`
-	Source    string         `yaml:"source"`
-	Targets   []string       `yaml:"targets"`
+	Source    string         `yaml:"source" env:"SOURCE"`
+	Targets   []string       `yaml:"targets" env:"TARGETS"`
 	Tokens    TokenConfig    `yaml:"tokens"`
 }
 
 // ColumnConfig represents the columns configuration
 type ColumnConfig struct {
-	Chapter string `yaml:"chapter"`
+	Chapter string `yaml:"chapter" env:"COLUMN_CHAPTER"`
 }
 
 // DatabaseConfig represents the database paths configuration
 type DatabaseConfig struct {
-	Calibre    string `yaml:"calibre"`
-	CalibreWeb string `yaml:"calibreweb"`
+	Calibre    string `yaml:"calibre" env:"DATABASE_CALIBRE"`
+	CalibreWeb string `yaml:"calibreweb" env:"DATABASE_CALIBREWEB"`
 }
 
 // TokenConfig represents the API tokens configuration
 type TokenConfig struct {
-	Anilist   string `yaml:"anilist"`
-	Hardcover string `yaml:"hardcover"`
+	Anilist   string `yaml:"anilist" env:"TOKEN_ANILIST"`
+	Hardcover string `yaml:"hardcover" env:"TOKEN_HARDCOVER"`
 }
 
 var (
